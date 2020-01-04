1348b37c74e682fece46e9b5743cdb2ef6c4a59e
Al <nguyenvtalexander@gmail.com>
2020-01-03 16:52:53 -0800

    game import

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     const canvas = document.getElementById("game");
     const ctx = canvas.getContext("2d");
+
+    const game = new Game();
+
 });
