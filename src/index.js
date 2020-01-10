ec9d5d036fb1c72c540d73f980175524c7884934
Al <nguyenvtalexander@gmail.com>
2020-01-10 15:17:37 -0800

    fixed asset management

--- src/index.js
+++ src/index.js
@@ -8,6 +8,19 @@ document.addEventListener('DOMContentLoaded', () => {
     const bottomBorder = document.querySelector('.checker');
     bottomBorder.style.backgroundImage = "url('assets/images/checker.png')";
 
+    const mario = new FontFace('mario', 'url(assets/fonts/Krungthep.ttf)')
+    const marioGalaxy = new FontFace('marioGalaxy', 'url(assets/fonts/SuperMario256.ttf)');
+
+    mario.load()
+        .then(loadedFace => {
+            document.fonts.add(loadedFace);
+        });
+
+    marioGalaxy.load()
+        .then(loadedFace => {
+            document.fonts.add(loadedFace);
+        });
+
     const startGame = e => {
         if (e.keyCode === 32) {
             const typeWriter = new Game(ctx, canvas, input);
@@ -15,6 +28,7 @@ document.addEventListener('DOMContentLoaded', () => {
             typeWriter.startGame();
 
             document.removeEventListener('keyup', startGame);
+
         };
     };
 
